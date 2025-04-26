575e025f5f6685c56da8153d3fecc3f96b03a4e3
Martin Ondiwa <96140915+martinondiwa@users.noreply.github.com>
2025-04-26 08:00:14 +0300

    Update config.py
    instance config

---
+++ instance/config.py
@@ -0,0 +1,6 @@
+# instance/config.py
+
+SECRET_KEY = 'your-secret-key-goes-here'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///cema_health.db'  # or a full PostgreSQL/MySQL URI
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+DEBUG = True
