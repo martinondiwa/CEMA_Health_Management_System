7876ee0d6eaba43c2e934de39e58654e33c98a06
Martin Ondiwa <96140915+martinondiwa@users.noreply.github.com>
2025-04-26 09:30:27 +0300

    Update auth_routes.py
    db

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required, current_user
 from app.models import User
-from app import db
+from app.extensions import db
 from app.forms import LoginForm, RegistrationForm
 from werkzeug.security import generate_password_hash, check_password_hash
 
