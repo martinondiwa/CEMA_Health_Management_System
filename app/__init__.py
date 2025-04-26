f71d6f4e1871aeca2c1cbd98bc8e988583aef0cf
Martin Ondiwa <96140915+martinondiwa@users.noreply.github.com>
2025-04-26 10:20:56 +0300

    Update __init__.py
    zcv

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,14 @@ def create_app(config_class=Config):
     login_manager.init_app(app)
     bcrypt.init_app(app)
 
+    # Set login view and message category
+login_manager.login_view = 'auth.login'
+login_manager.login_message_category = 'info'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+    
     # Import models for migrations
     from app import models
 
