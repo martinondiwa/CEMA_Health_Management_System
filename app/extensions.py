47c6101256bbcf3bde2d3b9b7a98d8af519cca50
Martin Ondiwa <96140915+martinondiwa@users.noreply.github.com>
2025-04-26 09:36:55 +0300

    Update extensions.py
    Alchemy

--- app/extensions.py
+++ app/extensions.py
@@ -1,6 +1,16 @@
-#app/extensions.py
+# app/extensions.py
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
+bcrypt = Bcrypt()
+
+login_manager.login_view = 'auth.login'
+login_manager.login_message_category = 'info'
+
 
