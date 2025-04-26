5527ad0d05d87c25eb71a19224ea0475c7e6d181
Martin Ondiwa <96140915+martinondiwa@users.noreply.github.com>
2025-04-26 12:59:00 +0300

    Update auth_routes.py
    asdd

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -12,15 +12,21 @@ auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-       return redirect(url_for('doctor.dashboard')) # Redirect to doctor dashboard if already logged in
+        if current_user.is_admin:
+            return redirect(url_for('admin.dashboard'))  # Or wherever admins go
+        return redirect(url_for('doctor.dashboard'))
 
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()  # Check if the email exists
-        if user and check_password_hash(user.password_hash, form.password.data):  # Use password_hash instead of password
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and check_password_hash(user.password_hash, form.password.data):
             login_user(user)
             flash('Login successful!', 'success')
-            return redirect(url_for('doctor.dashboard'))  # Redirect to doctor dashboard after successful login
+            
+            # Redirect based on role
+            if user.is_admin:
+                return redirect(url_for('admin.dashboard'))
+            return redirect(url_for('doctor.dashboard'))
         else:
             flash('Invalid login credentials. Please try again.', 'danger')
     return render_template('auth/login.html', form=form)
