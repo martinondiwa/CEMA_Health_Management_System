13fb94c8b5d83f959a90757d404131fc9e3cd9c5
Martin Ondiwa <96140915+martinondiwa@users.noreply.github.com>
2025-04-26 06:28:06 +0300

    Update config.py
    config

---
+++ app/config.py
@@ -0,0 +1,24 @@
+# app/config.py
+
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '..', '.env'))
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'you-should-set-a-secret-key')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, '..', 'instance', 'cema.db'))
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+
+class ProductionConfig(Config):
+    DEBUG = False
+    TESTING = False
