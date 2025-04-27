c2b52902a9c2f5aa8102484a869ff198d761a080
Martin Ondiwa <96140915+martinondiwa@users.noreply.github.com>
2025-04-27 22:26:38 +0300

    Update __init__.py
    adf

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,9 @@
-# tests/__init__.py
-
 import os
 from dotenv import load_dotenv
 
 # Load environment variables from .env in the project root
 load_dotenv()
 
-# Optional: Force test config mode
+#  Force test config mode
 os.environ["FLASK_ENV"] = "testing"
 os.environ["TESTING"] = "True"
